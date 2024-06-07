8f03b6fe1d1507f4e1afbc70e22b8e7ede5e4b0c
Beiri Alife <beiri.al@outlook.com>
2024-06-07 20:45:14 -0300

    Ep 06

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -24,7 +24,18 @@ class AuthController extends Controller
         $user = User::firstWhere('email', $request->email);
 
         return $this->ok('Authenticated', [
-            'token' => $user->createToken('API token for ' . $user->email)->plainTextToken
+            'token' => $user->createToken(
+                'API token for ' . $user->email,
+                ['*'],
+                now()->addMonth()
+            )->plainTextToken
         ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->ok('');
     }
 }
