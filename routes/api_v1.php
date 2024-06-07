d6586bca22e5b4e1a4ae973e483e768ab98b14dd
Beiri Alife <beiri.al@outlook.com>
2024-06-07 20:32:01 -0300

    Ep. 05

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -6,7 +6,7 @@ use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
